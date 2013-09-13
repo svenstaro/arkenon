e08087cedb921eff2906c1ced8e5534c3b31cdd9
rek <me.teichmann@gmail.com>
2013-09-13 13:09:05 +0200

    Converted tabs to spaces

--- cmake-extras/FindAlure.cmake
+++ cmake-extras/FindAlure.cmake
@@ -1,36 +1,35 @@
 # - Try to find Alure
 # Once done, this will define
-#
 # ALURE_FOUND - system has Alure
 # ALURE_INCLUDE_DIRS - the Alure Include directories
 # ALURE_LIBRARIES - link these to use Alure
 
 # find the Alure include directory
 find_path(ALURE_INCLUDE_DIR
-	NAMES
-		alure.h
-	PATH_SUFFIXES 
-		AL OpenAL
-	PATHS
-		/usr/include
-		/usr/local/include
-		/opt/local/include
-		/sw/include
-		${ALURE_DIR}/src/
+    NAMES
+        alure.h
+    PATH_SUFFIXES
+        AL OpenAL
+    PATHS
+        /usr/include
+        /usr/local/include
+        /opt/local/include
+        /sw/include
+        ${ALURE_DIR}/src/
 )
 
 # find the Alure library
 find_library(ALURE_LIBRARY
-	NAMES 
-		alure
-	PATH_SUFFIXES 
-		lib64 lib
-	PATHS 
-  		/usr
-    	/usr/local
-    	/opt/local
-    	/sw
-    	${ALURE_DIR}/lib
+    NAMES
+        alure
+    PATH_SUFFIXES
+        lib64 lib
+    PATHS
+        /usr
+        /usr/local
+        /opt/local
+        /sw
+        ${ALURE_DIR}/lib
 )
 
 # handle the QUIETLY and REQUIRED arguments and set ALURE_FOUND to TRUE if all listed variables are TRUE
