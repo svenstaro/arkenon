d34a43830b21ab1432c3a34c27a2e17402ac0e91
rek <me.teichmann@gmail.com>
2013-09-13 12:37:11 +0200

    Added new FindAlure.cmake

--- cmake-extras/FindAlure.cmake
+++ cmake-extras/FindAlure.cmake
@@ -1,66 +1,39 @@
 # - Try to find Alure
-# Once done this will define
+# Once done, this will define
 #
-#  ALURE_FOUND - system has Alure
-#  ALURE_INCLUDE_DIRS - the Alure include directory
-#  ALURE_LIBRARIES - Link these to use Alure
-#
-#  Copyright © 2011 Laszlo Papp <djszapi@archlinux.us>
-#
-#  Redistribution and use is allowed according to the terms of the New
-#  BSD license.
-#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
+# ALURE_FOUND - system has Alure
+# ALURE_INCLUDE_DIRS - the Alure Include directories
+# ALURE_LIBRARIES - link these to use Alure
 
+# find the Alure include directory
 find_path(ALURE_INCLUDE_DIR
-    NAMES
-      alure.h
-      PATH_SUFFIXES AL OpenAL
-    PATHS
-      /usr/include
-      /usr/local/include
-      /opt/local/include
-      /sw/include
-  )
+	NAMES
+		alure.h
+	PATH_SUFFIXES 
+		AL OpenAL
+	PATHS
+		/usr/include
+		/usr/local/include
+		/opt/local/include
+		/sw/include
+		${ALURE_DIR}/src/
+)
 
-#IF (NOT ALURE_MIN_VERSION)
-#  SET(ALURE_MIN_VERSION "1.1")
-#ENDIF(NOT ALURE_MIN_VERSION)
+# find the Alure library
+find_library(ALURE_LIBRARY
+	NAMES 
+		alure
+	PATH_SUFFIXES 
+		lib64 lib
+	PATHS 
+  		/usr
+    	/usr/local
+    	/opt/local
+    	/sw
+    	${ALURE_DIR}/lib
+)
 
-#SET(ALURE_VERSION_OK TRUE)
-#IF(ALURE_INCLUDE_DIR)
-#    FILE(READ ${ALURE_INCLUDE_DIR}/alure.h ALURE_VERSION_CONTENT)
-#    STRING (REGEX MATCH "ALURE_API.*.alureUpdateInterval" ALURE_VERSION_MATCH "${ALURE_VERSION_CONTENT}")
-#    IF(ALURE_VERSION_MATCH)
-#        SET(ALURE_VERSION "1.1")
-#    ELSE(ALURE_VERSION_MATCH)
-#        SET(ALURE_VERSION_OK FALSE)
-#        MESSAGE(STATUS "Can not find a recent enough version of Alure. Please install ${ALURE_MIN_VERSION} or newer. Found ${ALURE_VERSION}")
-#    ENDIF(ALURE_VERSION_MATCH)
-#ENDIF(ALURE_INCLUDE_DIR)
-
-
-  find_library(ALURE_LIBRARY
-    NAMES
-      alure alure 32 ALURE32-static ALURE32
-    PATHS
-      /usr/lib
-      /usr/local/lib
-      /opt/local/lib
-      /sw/lib
-  )
-
-  set(ALURE_INCLUDE_DIRS
-      ${ALURE_INCLUDE_DIR}
-  )
-  set(ALURE_LIBRARIES
-      ${ALURE_LIBRARY}
-  )
-
-  INCLUDE(FindPackageHandleStandardArgs)
-  # handle the QUIETLY and REQUIRED arguments and set ALURE_FOUND to TRUE if
-  # all listed variables are TRUE
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Alure DEFAULT_MSG ALURE_LIBRARY ALURE_INCLUDE_DIR)
-
-  # show the ALURE_INCLUDE_DIRS and ALURE_LIBRARIES variables only in the advanced view
-  mark_as_advanced(ALURE_INCLUDE_DIRS ALURE_LIBRARIES)
+# handle the QUIETLY and REQUIRED arguments and set ALURE_FOUND to TRUE if all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(ALURE DEFAULT_MSG ALURE_INCLUDE_DIR ALURE_LIBRARY)
+mark_as_advanced(ALURE_INCLUDE_DIR ALURE_LIBRARY)
